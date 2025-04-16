70a97619ef45d35faa412c512686f1361d7e5cce
rgurdzhian <ruben.gurdzhian@gmail.com>
2025-04-15 22:25:52 -0600

    initial utils and splash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
-import { Stack } from "expo-router";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
 
-export default function RootLayout() {
-  return <Stack />;
+const _layout = () => {
+    return (
+        <Stack screenOptions={{
+            headerShown: false
+        }}></Stack>
+    )
 }
+
+export default _layout
+
+const styles = StyleSheet.create({})
